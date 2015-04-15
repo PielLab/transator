7ee5d6e33f29aa529bdbd52e1c11e9f9cd60bcd7
Pablo <pablo.a.moreno@gmail.com>
2015-04-15 23:45:47 +0100

    Documents sequence feature.

--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/generator/SequenceFeature.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/generator/SequenceFeature.java
@@ -1,5 +1,7 @@
 package uk.ac.ebi.cheminformatics.pks.generator;
 
+import java.util.Collection;
+
 /**
  * Created with IntelliJ IDEA.
  * User: pmoreno
@@ -9,11 +11,33 @@ package uk.ac.ebi.cheminformatics.pks.generator;
  */
 public interface SequenceFeature {
 
+    /**
+     * Returns the PKMonomer associated with this sequence feature.
+     * This PKMonomer in turns contains the structure of the monomer
+     * associated to this sequence feature.
+     *
+     * @return PKMonomer with structure associated to this sequence feature.
+     */
     public PKMonomer getMonomer();
 
+    /**
+     * The name of the sequence feature, which in the case of the clade would
+     * correspond to the clade identifier.
+     *
+     * @return the name of the sequence feature.
+     */
     public String getName();
 
+    /**
+     * Whether the is a post processor assigned to this sequence feature.
+     *
+     * @return true if there is a post processor assigned.
+     */
     boolean hasPostProcessor();
 
+    /**
+     * Obtains the post processor (if any) associated to this clade.
+     * @return
+     */
     PostProcessor getPostProcessor();
 }
