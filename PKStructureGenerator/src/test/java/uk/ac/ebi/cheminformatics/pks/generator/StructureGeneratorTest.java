3526213bebc343e8ce8227dfc2beffc88e23d4c5
Pablo <pablo.a.moreno@gmail.com>
2015-04-15 15:09:56 +0100

    Adds test for non-extending KS based on onnamide features.

--- PKStructureGenerator/src/test/java/uk/ac/ebi/cheminformatics/pks/generator/StructureGeneratorTest.java
+++ PKStructureGenerator/src/test/java/uk/ac/ebi/cheminformatics/pks/generator/StructureGeneratorTest.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.cheminformatics.pks.generator;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.io.MDLV2000Writer;
@@ -16,13 +17,18 @@ import java.io.IOException;
  * To change this template use File | Settings | File Templates.
  */
 public class StructureGeneratorTest {
+
+    private static final Logger LOGGER = Logger.getLogger(PKStructure.class);
+
     @Test
     public void testGetStructure() throws Exception {
+        LOGGER.info("Starting cosmid sequence features test");
         runGenerator("Cosmidsequence.features", "/tmp/cosmid.mol");
     }
 
     @Test
     public void testGetStructureConcat() throws Exception {
+        LOGGER.info("Starting concat features test");
         String features = "concatenated.features";
         String molFileOut = "/tmp/concat.mol";
 
@@ -31,7 +37,14 @@ public class StructureGeneratorTest {
 
     @Test
     public void testGetStructureOnnamide() throws Exception {
+        LOGGER.info("Starting onnamide sequence features test");
         runGenerator("onnamid.features","/tmp/onnamidePK.mol");
+    }
+
+    @Test
+    public void testExtenderClade27ShortOnnamide() throws Exception {
+        LOGGER.info("Starting extender features test");
+        runGenerator("onnamidShortExtender27Test.features","/tmp/onnamideExtenderShort.mol");
     }
 
     private void runGenerator(String features, String molFileOut) throws IOException, CDKException {
