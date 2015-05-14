3f0b4ec6b2e0aebfd855ce2845344c7d01252234
Pablo <pablo.a.moreno@gmail.com>
2015-05-14 22:12:12 +0100

    Adds new structure loaders, to start moving the mol files outside of the jars.

---
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/io/AminoAcidStructLoader.java
@@ -0,0 +1,29 @@
+package uk.ac.ebi.cheminformatics.pks.io;
+
+import runner.RunnerPreferenceField;
+import uk.ac.ebi.cheminformatics.pks.PKSPreferences;
+
+import java.util.prefs.Preferences;
+
+/**
+ * Loads the mol file into an IAtomContainer for an amino acid from a file in the user's file system
+ * (as opposed to an internal file in the jar).
+ */
+public class AminoAcidStructLoader extends AbstractFileStructLoader implements StructureLoader {
+
+    Preferences prefs = Preferences.userNodeForPackage(PKSPreferences.class);
+
+    /**
+     * Inits the loader with the name of the amino acid, which needs to be exactly the name of the file
+     * plus the mol file extension to load. The directory is defined by the
+     * {@link RunnerPreferenceField.AminoAcidsMolsPath} field.
+     *
+     * @param aminoAcidName
+     */
+    public AminoAcidStructLoader(String aminoAcidName) {
+        this.cladeName = aminoAcidName;
+        StringBuilder builder = new StringBuilder(prefs.get(RunnerPreferenceField.AminoAcidsMolsPath.toString(), ""));
+        builder.append(aminoAcidName).append(".mol");
+        this.path = builder.toString();
+    }
+}
---
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/io/StructureLoader.java
@@ -0,0 +1,17 @@
+package uk.ac.ebi.cheminformatics.pks.io;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+
+/**
+ * Loader of Mol files, which can be for monomer, amino acids, stream based or file based.
+ */
+public interface StructureLoader {
+
+    /**
+     * Loads the structure defined for the loader.
+     *
+     * @return an {@link IAtomContainer} with the molecule.
+     */
+    public IAtomContainer loadStructure();
+
+}
