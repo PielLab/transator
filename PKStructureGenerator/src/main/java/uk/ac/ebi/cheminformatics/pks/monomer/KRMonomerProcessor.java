99060e4a5b9aba0ba5b31233b322f64c7d0339d6
Pablo Moreno <pablo.a.moreno@gmail.com>
2017-04-09 23:40:47 +0100

    Comments unimplemented exception for trials in docker. This needs to be reverted.

--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/monomer/KRMonomerProcessor.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/monomer/KRMonomerProcessor.java
@@ -10,7 +10,8 @@ package uk.ac.ebi.cheminformatics.pks.monomer;
 public class KRMonomerProcessor implements MonomerProcessor {
     @Override
     public void modify(PKMonomer monomer) {
-        throw new UnsupportedOperationException("Method not implemented yet!!!");
+        //TODO implement processor
+        //throw new UnsupportedOperationException("Method not implemented yet!!!");
     }
 
 }
