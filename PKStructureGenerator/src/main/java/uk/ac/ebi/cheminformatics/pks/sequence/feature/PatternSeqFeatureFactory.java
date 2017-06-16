46c73526fb4d280558c70a4134b7fb2b27e0988a
Alon Dolev <alon@dolev.ch>
2017-06-16 16:28:09 +0200

    removes intelliJ creation template

--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/sequence/feature/PatternSeqFeatureFactory.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/sequence/feature/PatternSeqFeatureFactory.java
@@ -2,13 +2,6 @@ package uk.ac.ebi.cheminformatics.pks.sequence.feature;
 
 import uk.ac.ebi.cheminformatics.pks.parser.FeatureFileLine;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmoreno
- * Date: 4/7/13
- * Time: 15:06
- * To change this template use File | Settings | File Templates.
- */
 public class PatternSeqFeatureFactory {
     public static SequenceFeature makeSequenceFeature(FeatureFileLine featureFileLine) {
         return new ProteinPatternSeqFeature(featureFileLine);
