16df8d730d0bd59a9774172481c9c2ec22d2ee09
Alon Dolev <alon@dolev.ch>
2017-07-19 12:53:07 +0200

    cleans up verifier

--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/verifier/SingleConnectedComponentVerifier.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/verifier/SingleConnectedComponentVerifier.java
@@ -2,25 +2,17 @@ package uk.ac.ebi.cheminformatics.pks.verifier;
 
 import org.openscience.cdk.graph.ConnectivityChecker;
 import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.interfaces.IBond;
 import uk.ac.ebi.cheminformatics.pks.generator.PKStructure;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmoreno
- * Date: 11/6/15
- * Time: 23:31
- * To change this template use File | Settings | File Templates.
- */
 public class SingleConnectedComponentVerifier implements Verifier {
     @Override
-    public boolean verify(PKStructure struc) {
-        IAtomContainer mol = struc.getMolecule();
+    public boolean verify(PKStructure structure) {
+        IAtomContainer mol = structure.getMolecule();
         return !ConnectivityChecker.isConnected(mol);
     }
 
     @Override
     public String descriptionMessage() {
-        return "Disconnected components found";  //To change body of implemented methods use File | Settings | File Templates.
+        return "Disconnected components found";
     }
 }
--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/verifier/Verifier.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/verifier/Verifier.java
@@ -2,24 +2,18 @@ package uk.ac.ebi.cheminformatics.pks.verifier;
 
 import uk.ac.ebi.cheminformatics.pks.generator.PKStructure;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmoreno
- * Date: 11/6/15
- * Time: 23:29
- * To change this template use File | Settings | File Templates.
- */
+
 public interface Verifier {
 
     /**
      * Runs implemented verification, producing true if the error searched for
      * is present.
      *
-     * @param struc
+     * @param structure
      * @return true if the feature is present.
      */
-    public boolean verify(PKStructure struc);
+    boolean verify(PKStructure structure);
 
-    public String descriptionMessage();
+    String descriptionMessage();
 
 }
