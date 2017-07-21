15ba0ad35ae131e6d7a19f69f6feefa25cfb548f
Alon Dolev <alon@dolev.ch>
2017-07-21 14:11:10 +0200

    format code

--- PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/io/AbstractFileStructLoader.java
+++ PKStructureGenerator/src/main/java/uk/ac/ebi/cheminformatics/pks/io/AbstractFileStructLoader.java
@@ -1,6 +1,5 @@
 package uk.ac.ebi.cheminformatics.pks.io;
 
-import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.io.MDLV2000Reader;
 import org.openscience.cdk.silent.SilentChemObjectBuilder;
@@ -8,22 +7,14 @@ import org.openscience.cdk.tools.CDKHydrogenAdder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 
 import java.io.FileReader;
-import java.io.IOException;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmoreno
- * Date: 12/5/15
- * Time: 23:55
- * To change this template use File | Settings | File Templates.
- */
 public abstract class AbstractFileStructLoader {
 
     String path;
     String cladeName;
     String errorLabel = "Could not read molecule for PKS Clade monomer: ";
 
-    public IAtomContainer loadStructure() throws Exception{
+    public IAtomContainer loadStructure() throws Exception {
         MDLV2000Reader reader =
                 new MDLV2000Reader(new FileReader(this.path));
         IAtomContainer mol = reader.read(SilentChemObjectBuilder.getInstance().newInstance(IAtomContainer.class));
