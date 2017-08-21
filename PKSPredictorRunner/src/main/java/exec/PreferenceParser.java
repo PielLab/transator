81778ef387d65043f2d61e89238beba586e42642
Alon Dolev <alon@dolev.ch>
2017-08-21 13:52:54 +0200

    formats file

--- PKSPredictorRunner/src/main/java/exec/PreferenceParser.java
+++ PKSPredictorRunner/src/main/java/exec/PreferenceParser.java
@@ -5,21 +5,14 @@ import runner.RunnerPreferenceField;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: pmoreno
- * Date: 25/6/14
- * Time: 20:03
- * To change this template use File | Settings | File Templates.
- */
 public class PreferenceParser {
 
-    public Map<Enum,String> parse(String arguments) {
+    public Map<Enum, String> parse(String arguments) {
         String pairs[] = arguments.split(";");
-        Map<Enum,String> map = new HashMap<Enum, String>();
+        Map<Enum, String> map = new HashMap<>();
         for (String pair : pairs) {
             String[] token = pair.split(":");
-            map.put(RunnerPreferenceField.valueOf(token[0]),token.length==2 ? token[1] : "");
+            map.put(RunnerPreferenceField.valueOf(token[0]), token.length == 2 ? token[1] : "");
         }
         return map;
     }
